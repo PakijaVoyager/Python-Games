61e456f2eac7532ba45020e8780ce98248a97301
Junat S'Imsuk <tanujkusmi1@gmail.com>
2025-02-05 17:33:02 +0530

    Add files via upload
    This is the simple program which select a number and allows it's users to guess it and count it.

---
+++ Number guesser.py
@@ -0,0 +1,15 @@
+from random import randint
+
+count = 0
+ask_usr = 0
+guess = randint(1,10)
+print(guess)
+while guess != ask_usr:
+    ask_usr = int(input("Guess the number : "))
+    if ask_usr > guess:
+        print("You are above the number")
+        count+=1
+    elif ask_usr < guess:
+        print("You are below the number")
+        count+=1
+print("You get it in",count,"times")
